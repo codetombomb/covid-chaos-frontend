9c682ec6a0b2630ab9b556c387d1e870a724a6d3
Tom Tobar <codetombomb@gmail.com>
2020-11-06 16:25:41 -0600

    Replace deprecated keyCode

--- public/js/controls.js
+++ public/js/controls.js
@@ -1,36 +1,37 @@
 const keysDown = {};
 
 window.addEventListener("keydown", function (e) {
-    if ([37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+    console.log(e.key, e.keyCode, keysDown)
+    if (["ArrowLeft", "ArrowUp", "ArrowRight", "ArrowDown"].indexOf(e.key) > -1) {
         e.preventDefault();
     }
 }, false);
 
 window.addEventListener('keydown', function (e) {
-    keysDown[e.keyCode] = true;
+    keysDown[e.key] = true;
 });
 
 window.addEventListener('keyup', function (e) {
-    delete keysDown[e.keyCode];
+    delete keysDown[e.key];
 });
 
 const movePlayer = (mod) => {
-    if (37 in keysDown) { // Player holding left
+    if ("ArrowLeft" in keysDown) { // Player holding left
         if (player.x > 0) {
             player.x -= player.speed * mod;
         }
     }
-    if (38 in keysDown) { // Player holding up
+    if ("ArrowUp" in keysDown) { // Player holding up
         if (player.y > 0) {
             player.y -= player.speed * mod;
         }
     }
-    if (39 in keysDown) { // Player holding right
+    if ("ArrowRight" in keysDown) { // Player holding right
         if (player.x < 556) {
             player.x += player.speed * mod;
         }
     }
-    if (40 in keysDown) { // Player holding down
+    if ("ArrowDown" in keysDown) { // Player holding down
         if (player.y < 356) {
             player.y += player.speed * mod;
         }
