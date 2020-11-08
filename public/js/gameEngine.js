cfacd87045357fb3c71e306779b6158345bd417b
Tom Tobar <codetombomb@gmail.com>
2020-11-07 22:30:49 -0600

    Add Tp

--- public/js/gameEngine.js
+++ public/js/gameEngine.js
@@ -13,21 +13,23 @@ const update = () => {
     render();
     movePlayer(0.02);
     getSanitizer();
+    getTp();
 }
 
 const render = () => {
     ctx.drawImage(bgImage, 0, 0);
     ctx.drawImage(playerImage, player.x, player.y, player.width, player.height);
-    ctx.drawImage(sanitizerImage, sanitizerAttrs.x, sanitizerAttrs.y, sanitizerAttrs.width, sanitizerAttrs.height)
+    ctx.drawImage(sanitizerImage, sanitizerAttrs.x, sanitizerAttrs.y, sanitizerAttrs.width, sanitizerAttrs.height);
+    ctx.drawImage(tpImage, tpAttrs.x, tpAttrs.y, tpAttrs.width, tpAttrs.height);
     for (let i = 0; i < virusAttrs.quantity; i++) {
         virusAttrs.store[i].show();
         virusAttrs.store[i].fall();
-    };
+    }
 
     for (let i = 0; i < zombieAttrs.quantity; i++) {
         zombieAttrs.store[i].show();
         zombieAttrs.store[i].fall();
-    };
+    }
 }
 
 const stopGame = () => {
