08d034702ffa767b86c3ac99cf4bc87c2850ec3c
Tom Tobar <codetombomb@gmail.com>
2020-11-07 22:14:34 -0600

    Add sanitizer to gameplay

--- public/js/gameEngine.js
+++ public/js/gameEngine.js
@@ -4,6 +4,7 @@ const startGame = () => {
     makeZombies();
     makeViruses();
     update();
+    sneeze.play();
     mainMusic.play();
 }
 
@@ -11,11 +12,13 @@ const update = () => {
     stopId = window.requestAnimationFrame(update);
     render();
     movePlayer(0.02);
+    getSanitizer();
 }
 
 const render = () => {
     ctx.drawImage(bgImage, 0, 0);
     ctx.drawImage(playerImage, player.x, player.y, player.width, player.height);
+    ctx.drawImage(sanitizerImage, sanitizerAttrs.x, sanitizerAttrs.y, sanitizerAttrs.width, sanitizerAttrs.height)
     for (let i = 0; i < virusAttrs.quantity; i++) {
         virusAttrs.store[i].show();
         virusAttrs.store[i].fall();
